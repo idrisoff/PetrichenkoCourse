082911959b16dbbdfc1bc0594339eeb75f7745e0
idrisoff <idrisoff1986@mail.ru>
2020-08-30 21:45:12 +0500

    lesson_6

---
+++ budget-app/src/js/main.js
@@ -0,0 +1,28 @@
+start = document.querySelector('#start')
+budget = document.querySelector('.budget-value')
+daybudget = document.querySelector('.daybudget-value')
+level = document.querySelector('.level-value')
+expenses = document.querySelector('.expenses-value')
+optionalexpenses = document.querySelector('.optionalexpenses-value')
+income = document.querySelector('.income-value')
+monthsavings = document.querySelector('.monthsavings-value')
+yearsavings = document.querySelector('.yearsavings-value')
+year = document.querySelector('.year-value')
+month = document.querySelector('.month-value')
+day = document.querySelector('.day-value')
+
+input1 = document.querySelector('.expenses-item')
+input2 = document.querySelector('.expenses-item')
+input3 = document.querySelector('.expenses-item')
+input4 = document.querySelector('.expenses-item')
+
+utverdit = document.querySelector('button')
+utverdit = document.querySelector('button')
+raschitat = document.querySelector('button')
+
+neob_rashod = document.querySelectorAll('.optionalexpenses-item')
+
+choose_income = document.querySelector('#income')
+checkbox = document.querySelector('#savings')
+sum = document.querySelector('#sum')
+percent = document.querySelector('#percent')
